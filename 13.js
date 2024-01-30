4c4884bf4745feb86d09d9f5b94aa9046bfdd49d
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-01-30 17:30:12 +0700

    13.js

---
+++ 13.js
@@ -0,0 +1,36 @@
+// programming session 15, 15 minutes (#13)
+// date: Jan 30 2024 
+
+// pop = take something out of the stack, as in popping out 
+// push = adding something to the stack, as in pushing in
+
+// declare the array 
+let blackPinkMembers = ["jenny", "jisoo", "rose", "lisa"];
+
+// check what's inside the array 
+console.log("Black Pink member lineup: ", blackPinkMembers);
+
+// time for surprise stage solo 
+// create new variable and use pop method for blackPinkMmebers array 
+let surpriseSolo = blackPinkMembers.pop();
+
+// tell who is the soloist 
+console.log(surpriseSolo);
+
+// check what's inside the array 
+console.log("Black Pink member lineup: ", blackPinkMembers);
+
+// LIFO, last in, first out. 
+// draw a chart so you remember the concept 
+// before: (1) (2) (3) (4) people entered the bus 
+// the (4) person was the last one to enter the bus
+// the (4) person will be kicked out of the bus
+// therefore, it's last in, first out 
+// just like in the pop() example
+// oh god where is my lisa
+// fr fr no cap 
+// this line: let surpriseSolo = blackPinkMembers.pop();
+// kicked out lisa of the line up
+// fr fr no cap oh god 
+// how can can pop() do this to lisa 
+// another way to visualize 
