41058d5f75defc3096133ad09e10d81e17e6230e
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-01-31 20:37:32 +0700

    23.js

---
+++ 23.js
@@ -0,0 +1,76 @@
+// Programming session: 23
+// Session: 20 minutes
+// Date: Jan 31 2024
+
+// functions, I have to learn what it is and get good at it 
+// function as a block of use-able code that you can use over and over again
+// and call it up like a booty call huh 
+
+// declare the function
+// give it the name highFive
+// then the body (block code?) of the function being the console.log output
+// no return value ... yet 
+// still have to call the fucking function, don't forget that
+// a function is like a booty call, you have to CALL IT.
+
+function highFive() {
+    console.log("giving you a fucking high-five")
+}
+
+// calling the function
+// good the code works flawlessly 
+highFive()
+
+// let's do it again, so you can remember the syntax of the function
+
+function bootyCall() {
+    console.log("BRR BRR. RING RING. SUMMON THE BOOTY")
+}
+
+// call the function ... good
+bootyCall()
+
+// let's move on and learn about more about the function
+// what does the return value do and shit 
+// so the return keyword, is function is done doing whatever its doing
+// then it returns the value ... but to who and where? 
+
+function returnValue() {
+    return 42; 
+}
+
+// caught myself with a typo there, case sensitive 
+// okay the code returns a value of 42 .. but how? 
+// let's ask Llama to explain
+console.log(returnValue())
+
+// so the return function is like ordering food in a restaurant
+// and the chef gives you the meal ... mmm okay.
+// let's ask Llama to give me a parameter in the function 
+
+// declare a function, giving it the name "hello"
+// add the parameter (name) to the function hello 
+// the block of code is just a simple console.log function 
+// but using template literal because why the fuck not
+// template literal is more cool than just regular ""
+function hello(name) {
+    console.log(`yo, what's up ${name}`)
+}
+
+// there is a parameter 
+// I can assign any datatype inside the parameter it seems
+// interesting. 
+
+hello(5)
+
+// function hello(name) <--- (name) is the parameter 
+// hell("john") <--- "john" is the argument
+
+
+
+
+
+
+
+
+
