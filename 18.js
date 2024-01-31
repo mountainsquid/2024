67aa01fa315966e0f346f199e4baad7ce6857645
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-01-31 14:32:25 +0700

    18.js

--- 18.js
+++ 18.js
@@ -1,1 +1,36 @@
+// programming session 15, 20 minutes (#18)
+// date: Jan 31 2024 
 
+// boolean, boolean 
+// booelan is true or false, is there anything else? 
+const is_subbed = true; 
+const watched_all_mvs = true; 
+
+// making a new const 
+// where it holds the conditional of two other values 
+// god damn 
+const has_access = is_subbed && watched_all_mvs; 
+
+console.log("blink is subbed: ", is_subbed)
+console.log("blink has watched all MVs: ", watched_all_mvs)
+console.log("has subbed and watched all MVs: ", has_access)
+
+// define the value first with let 
+let daLat = "rainy" 
+
+// if, else if, else 
+// unlinimited else if right?
+// I am proud that I figured out if, else if, else, as a brainlet 
+// instead of using endless else if, go use switch man
+
+if (daLat == "cold") {
+    console.log("Da Lat is cold right now") 
+} else if (daLat == "warm") {
+    console.log("Da lat is warm feels good man")
+} else if (daLat == "rainy") {
+    console.log("Da Lat is rainy right now")
+} else {
+    console.log("I don't know about the weather mang")
+}
+
+// if, else if, else 
