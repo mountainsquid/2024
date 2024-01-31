962746762b36db9e4c7fe124dc0e88a971cb8ccc
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-01-31 13:55:43 +0700

    17.js

---
+++ 17.js
@@ -0,0 +1,22 @@
+// programming session 15, 20 minutes (#17)
+// date: Jan 31 2024 
+
+// using boolean
+// is the man gay
+// yeah the man is gay 
+
+let manIsGay = true;
+
+if (manIsGay) { 
+    console.log(manIsGay)
+}
+
+// set boolean to true 
+let roseIsPresent = true;
+
+// print out if Rose is at the concert 
+console.log("is Rose at the concert: ", roseIsPresent)
+
+let lisaIsPresent = true;
+let jisooIsPresent = true; 
+let jennyIsPresent = true; 
