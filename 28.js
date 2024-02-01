cca33f01c28dc1b9b0c1f8471405565b704455ec
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-01 11:54:23 +0700

    28.js

---
+++ 28.js
@@ -0,0 +1,65 @@
+// Programming session: 28
+// Date: Feb 01 2024
+// Session: 30 minutes
+
+// I need to master functions 
+// because that's the most basic building block
+// or rather something cool to keep reusing the code 
+// declare the function
+// call the function 
+// nothing happens until you call the function 
+// right, continue where I left off  
+// function is a global object? 
+// local scope = variable exist only inside the function or the block of code
+// globe scope = can use throughout the program 
+
+// okay this code is working okay so far 
+
+
+// I made the silly mistake of not using the let or var keyword
+// I think there was a brain fart of some sort but that's okay
+// ChatGPT fixed it for me
+
+function eat() {
+    let y = 11;
+    console.log("this is the local scope only: ", y)
+}
+
+eat()
+
+// this is the global scope
+// global scope is where you can use it anywhere in the program
+// local scope is where the variable only exist inside the function ... (and perhaps anywhere else)
+
+let y = 10 
+console.log('this is y global scope: ', y)
+
+// 15 minutes of learning done
+// let's try to think of some cool code and ask Poe for the answers
+// then try to learn the code because why not 
+
+// there is something that I forget when it comes to function 
+// is declaring the variables in the function
+// therefore I need to put in the repetition so it becomes muscle memory to put 
+// let, var, const
+// don't forget my son 
+
+function bigPenis() { 
+    let penis1 = 10
+    let penis2 = 11
+    let penis3 = 13
+
+    console.log("size of 1st penis: ", penis1)
+    console.log("size of 2nd penis: ", penis2)
+    console.log("size of 3rd penis: ", penis3)
+}
+
+bigPenis()
+
+// bro, so a constructor can construct an object? 
+// my mind is blown ...
+// the constructor is summoned by using a this keyword. 
+// interesting.
+
+// that's all the learning for this 30 minute session
+// see you in the next session. 
