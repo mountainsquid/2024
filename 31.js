e70edb2519d6a1e8a271c8ec9cce643705e105a0
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-02 08:45:23 +0700

    31.js

---
+++ 31.js
@@ -0,0 +1,82 @@
+// Programming session: 31
+// Date: Feb 02 2024
+// Session: 30 minutes
+
+console.log("hello")
+
+// time to learn more about the switches 
+// you can't go wrong with learning about the switches
+// I need to refresh on the syntax a bit 
+// okay so it seems I've refreshed on switch case and the layer goes deeper
+// first there is the if, else if, else statements
+// then it evolved into switch case statements
+// then it evolved into ternary operator
+// like an evovling pokemon it keeps evolving :) 
+
+let benis = 5; 
+
+switch (benis == 5) {
+    case 5: 
+    console.log("ben is 5 inches")
+    break
+    default: 
+    console.log("ben size is not confirmed")
+}
+
+// here we define the age of the car
+// in this case you have a 5 year old car 
+let carAge = 5 
+
+// gonna hit it up with the if statement
+// reason why is because we want some conditional flow for this shieeet
+// if the car is old, I'm going to throw it in the garbage
+// if it's a car less than 5 years old then keep driving 
+
+// first line being car is greater or equal to 5? 
+// then spite something out 
+// but what if the car is not that expression?
+// then we hit them with something else with the "else" part 
+// okay this is fine but what about the else if ?? I want an example of that 
+
+if (carAge >= 5) {
+    console.log("your car is 5 or more years old, it's time to get a new car")
+} else {
+    console.log("your car is doing great")
+}
+
+// learning how to do if, else if, else statement
+// seems that once it hits the conditional as true, then the if conditional stops
+// it doesn't even consider the else if with this code ... 
+
+let bench = 200
+
+if (bench >= 134) {
+    console.log("you're a beast! you're lifting more than 134 lbs")
+} else if (bench >= 200) {
+    console.log("bro, you can lift more than 200 lbs you're insane")
+} else {
+    console.log("it's time to train harder, every rep counts")
+}
+
+// for the if logical stack
+// seems like you want to code something greatest chance of being true
+// because once something becomes true, the if conditional stops at that block
+// if that makes sense 
+
+let squat = 90
+
+if (squat >= 200) {
+    console.log("you're a beast! You're squatting 200 lbs") 
+} else if (squat >= 150) { 
+    console.log("good work, you're squatting more than 150 lbs")
+} else {
+    console.log("train harder my friend")
+}
+
+// so far I've remember if, else if, else statement
+// then switch statement 
+// next up, I need to refresh on the while loop 
+// if you combine if and while loop it seems powerful 
+// keep grinding. 
+
+// that's all the coding for this session, see you in the next sesssion :)
