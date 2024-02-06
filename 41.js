d36d6d61b1e5f003fb211b585f987b85c1f620a7
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-06 11:22:06 +0700

    41.js

---
+++ 41.js
@@ -0,0 +1,50 @@
+// Programming session: 41
+// Date: Feb 06 2024
+// Session: 45 minutes
+
+// I need to brush on my while loop 
+
+// let i = 5 
+// while(i <= 5) { 
+//     console.log(i)
+//     i++
+// }
+
+// let a = 1 
+// while(a <= 5 ) {
+//     console.log("counting down A: ", a)
+//     a++
+// }
+
+// // set b to 3 
+// // while b is greater than 0 
+// // b-- each time 
+
+// let b = 3
+// while(b > 0) {
+//     console.log("counting down b: ", b)
+//     b--    
+// }
+
+// stacy has 5 lifetime sex partners 
+// she went to the bar and she's looking to bang
+// everytime she has sex with a new guy, the counter goes up by 1 
+// first decide stacy's sex count
+// then go for the while loop
+// while loop always have block code so remember the {}
+// after the console log, perform the stacy++ AFTER the console.log is one 
+
+// use case for loop, looping through an array and printing out all the values 
+// searching and sorting algos 
+// 
+let stacy = 5
+while (stacy <= 10) {
+    console.log("stacy partner countdown: ", stacy)
+    stacy++
+}
+
+// my brains feel like it's melting and I can't seem to code
+// still experiencing brain fog, perhaps this is from drinking coffee? 
+// extends keyword establishes the inheritance relationship
+// new keyword is to create an instance of a class
+// extends keyword is to create a establish a inheritance from 1 class to aother 
