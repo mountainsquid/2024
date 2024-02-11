9614489a38a9a5d476c240ff69264a32182f41f8
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-11 21:59:52 +0700

    51.sol

---
+++ 51.sol
@@ -0,0 +1,23 @@
+// Programming session: 51
+// Date: Feb 11 2024
+// Session: 30 minutes
+
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19; // stating our verison of solidity
+
+// this is a valid contract that doesn't do anything heh 
+// data types, boolean, uint, int, address, bytes 
+// uint can be positive or negative number huh 
+// uint gets defaulted to 0 don't forget that bruh 
+ // what is a public function in .sol learn that bruh 
+contract SimpleStorage { 
+    bool shemaleOrNot = true; 
+    uint favoriteNum = 13; 
+    uint256 herAge = -23; 
+    string greeting = "hello bitch";
+    address myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    
+
+}
+
+
