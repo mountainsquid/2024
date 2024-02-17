363c80534ea44a1e0c29d0139e36b01495364e39
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-17 12:28:13 +0700

    64.js

---
+++ 64.js
@@ -0,0 +1,22 @@
+// Programming session: 63
+// Date: Feb 16 2024
+// Session: 30 minutes
+
+// in this code, I am assigning the name lisaName to lisa 
+// then I'm using a built in method that converts to upper case 
+// but is there a more neater way to write this code? 
+// I am learning something new every day, feels good man 
+
+let lisaName = "lisa".toUpperCase()
+
+console.log(lisaName)
+
+// using array into the string literal, feels good man 
+
+let food = ["pizza", "burger", "fries", "pita"]
+var ready = "I am so hungry and I'm ready to eat: "
+
+// using for loop, to loop through 
+// the logic is, using food.length and using i++ to loop through each index 
+for (i = 0; i < food.length; i++)
+console.log(`${ready} for ${food[i]}`)
