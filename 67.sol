49203d35ea25db7ffe0badd8ecbf8519b27e2063
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-17 19:39:47 +0700

    67.sol

---
+++ 67.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: UNLICENSED
+// Programming session: 67
+// Date: Feb 17 2024
+// Session: 30 minutes
+
+// a structure is similiar to an object literal in javascript, therefore its not entirely unique 
+
+
+pragma solidity ^0.8.0;
+
+contract twitter { 
+
+    // creating my first struct in solidity, feels good man
+    // a struct is an object literal in Javascript 
+    // but what good is a struct for?
+    // store data, property access, ""dynamic structure"" whatever the fuck that means 
+
+    struct blackPinkMembers { 
+
+        // declare the struct because why the fuck not 
+        string blackName; 
+        uint256 age; 
+        string position; 
+    }
+
+
+}
+
+// mapping is key:pair value, like a dictionary or phone book
+// this sounds really similar in javascript ... 
+// remember "key value pair" and you should be okay 
+// practical example of key value pair? Mapping an 0x address to a name 
+// using mapping to find the key:value pair fast!
+// => arrow function, think it as "to" something. At least that's what the youtuber said 
+// no, => is a mapping operator ... that's interesting 
+// array as simply a list of things 
+// but it's the index that you get to access the array, feels good man 
+
+
