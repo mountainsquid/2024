a40b6742ab3d0c6c75914404d6f53dda73aec56b
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-20 22:29:21 +0700

    77.js

---
+++ 77.js
@@ -0,0 +1,31 @@
+// Programming session: 77
+// Date: Feb 20 2024
+// Session: 30 minutes
+
+// all keys are strings in object literal wtf bro 
+// key is the name: 
+// value is the "Jenny"
+// the key is the string, is that so? 
+
+const blackPink = { 
+    name: "Jenny",
+    age: 25, 
+    position: "singer"  
+
+}
+
+// different methods to changing the value, interesting 
+// changes the value of the key value pair 
+blackPink["name"] = "Lisa"
+
+// change the age of of key value pair 
+blackPink.age = 26
+
+// access object key value using (.) notation 
+console.log(blackPink.name)
+console.log(blackPink.age)
+
+// what is the use of object literal?
+// obviously to have some sort of database that you can store 
+// for example a hospital database where you can store patient's info 
+
