163d8d385f08e800ca79b921c5ae9059c0c1406a
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-20 16:42:24 +0700

    75.js

---
+++ 75.js
@@ -0,0 +1,34 @@
+// Programming session: 75
+// Date: Feb 20 2024
+// Session: 30 minutes
+
+let lisaHello = () => {
+    console.log("hey it's Lisa")
+}
+
+lisaHello() 
+
+let letMePoo = (a, b) => {
+    return a + b     
+}
+
+console.log(letMePoo(5,5))
+
+const lisaSing = () => { 
+    console.log("lisa is singing")
+}
+
+lisaSing()
+
+const fight = (a, b) => {
+    return a - b
+}
+
+console.log(fight(5,5))
+
+// parameter is when you declare the function
+// argument is when you pass values into the parameters 
+// NaN = not a number 
+// return keyword only reuseable inside a function, interesting didn't think of that 
+// once you learn how to code, you can't undo the seeing 
+
