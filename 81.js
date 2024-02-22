b3d007e0162044b5a7aaa4612464c62a47c27cf7
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-22 17:33:37 +0700

    81.js

---
+++ 81.js
@@ -0,0 +1,43 @@
+// Programming session: 81
+// Date: Feb 22 2024
+// Session: 30 minutes
+
+// create promise by creating a promise constructor 
+// a promise helps with async programming 
+
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: 172,
+        mass: 77,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: 202,
+        mass: 136,
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: 150,
+        mass: 49,
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: 188,
+        mass: 84,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+];
+
+//***MAP***
+// array map is when you take an array, and create an new array using .map() is that correct?
+
+const names = characters.map((characters) => characters.name) 
+console.log(names)
