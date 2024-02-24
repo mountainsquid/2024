7f390ac6b3b1049484ba226cf19b784f51c16b28
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-24 17:09:24 +0700

    Create 85.js

---
+++ 85.js
@@ -0,0 +1,17 @@
+// Programming session: 85
+// Date: Feb 24 2024
+// Session: 30 minutes
+
+// there are nodes and element in the DOM 
+// browser creates the DOM tree
+// but why DOM? make the website interactive 
+// DOM works across different browsers 
+// the internet without the DOM was boring 
+
+// in the HTML page, created <p id = "hello">THIS myPagraph original message getElementbyID</p>
+// then I changed the color of the paragraph by using this code below  
+
+document.getElementById('hello').style.color = 'red';
+
+// need to learn more about class and class factory in javascript 
+// for class, there this. and new when creating class 
