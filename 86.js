81597b8eb1203d1956a150c9b43cbecb1ef4896e
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-25 12:34:30 +0700

    Learning about encapsulation

---
+++ 86.js
@@ -0,0 +1,16 @@
+// Programming session: 86
+// Date: Feb 25 2024
+// Session: 30 minutes
+
+// time to learn encapsulation
+// I already learned how to create and inherit a class
+// learn the big 4 concepts of OOP coding
+// encapsulation = wrapping everything into a single unit.
+// encapsulation = wrapping the code (functions variables) into a single unit 
+
+// getting into more intermediate topics like class and the 4 big concepts behind it 
+
+function Lisa(herName) { 
+    const height = 144 // private property 
+
+ }
