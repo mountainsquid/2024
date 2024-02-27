779cbf8fec34c4b0ce562866057aacc2995e0517
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-27 13:08:32 +0700

    Keep grinding out that OOP knowledge

---
+++ 91.js
@@ -0,0 +1,26 @@
+// Programming session: 91
+// Date: Feb 26 2024
+// Session: 30 minutes
+
+// time to learn about classes 
+// and fully grasps the concepts but in code too 
+// when you create an object from a class (blueprint) it's called an instance 
+// remember about Pascal case my son 
+// software engineering is about storing data in the memory, then using function to ...
+// do something with that data. simple as
+
+
+class bigButt {  // creating the class called bigButt
+    constructor(ass) {  // have to create the class to substaniate the object. also arugment binding
+        this.ass = ass
+    }
+
+    drill() { 
+        console.log("I am drilling something. maybe a butt")
+    }
+}
+
+let lisaButt = new bigButt("Lisa's ass")
+
+console.log(lisaButt)
+lisaButt.drill()
