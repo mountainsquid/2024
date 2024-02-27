ae33ef603f2e02ba5b260c2d1f389c966dc9b588
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-02-27 13:48:49 +0700

    OOP

---
+++ 92.js
@@ -0,0 +1,30 @@
+// Programming session: 92
+// Date: Feb 26 2024
+// Session: 30 minutes
+
+// javascript single thread language, can process only 1 thing at a time 
+// thread of execution, going line by line, top down
+// but Javascript can be async by using event loop feels okay mang 
+// class, blueprint
+// object, born out of class's arse
+// call stack, has something to do with functions being run in order, some shit like that 
+// what's the point of learning about call stack anyways 
+// function expression over function declaration remember that my son
+// garbage collection = remove data from memory
+// 
+
+
+class bigButt {  // creating the class called bigButt
+    constructor(ass) {  // have to create the class to substaniate the object. also arugment binding
+        this.ass = ass
+    }
+
+    drill() { 
+        console.log("I am drilling something. maybe a butt")
+    }
+}
+
+let lisaButt = new bigButt("Lisa's ass")
+
+console.log(lisaButt)
+lisaButt.drill()
