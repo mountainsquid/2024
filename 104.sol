b0ed6c35fd29bed08cacad7adc63699cd1855adb
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-03-02 21:01:23 +0700

    required keyword

--- 104.sol
+++ 104.sol
@@ -1,1 +1,22 @@
 
+
+//SPDX-License-Identifier: UNLICENSED
+// Programming session: 104
+// Date: MARCH 02 2024
+// Session: 30 minutes
+pragma solidity ^0.8.0; 
+
+// contactor, only get run once (for whatever reason, because ...?)
+// constructor is gets run during the creation of the contract that's why
+// require keyword = requires specific condition before the code runs 
+
+contract eatingNuts { 
+
+    function checkMyNuts(uint a) public pure returns (bool) {
+        require(a==0, "a is indeed 0");
+        return true;
+    }
+
+}
+
+
