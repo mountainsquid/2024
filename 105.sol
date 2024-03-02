a1187f7792bf9ea36b4b8d72f56133ef39bd4fc6
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-03-03 00:37:55 +0700

    struct. all about it.

---
+++ 105.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: UNLICENSED
+
+// Programming session: 105
+// Date: MARCH 02 2024
+// Session: 30 minutes
+
+pragma solidity ^0.8.0; 
+
+// struct as blueprint and not state variable huh 
+// Durrrr i am creating da structs .... 
+// after I'm done learing struct, I'll move on to mapping 
+// I have a bunch of variables in the struct, now what
+
+contract eatingNuts { 
+    struct lisaGirl { 
+        string name;
+    }
+    
+    struct gIdle { 
+        uint256 members;
+        int8 disbutedYear;
+        bool farts;
+        uint[] smelly; 
+        int[] songs; 
+        int[] addresses;
+        uint[] yes;
+        bool paidYet;
+        uint8 phoneNumber;
+        int8 herPhone;
+        int8[] bankAccount; 
+        string peePee;
+        string pooPoo;
+        string peePeePooPoo;
+        string greetings;
+        string seeYou;
+        int128[] bankNumbers;
+        int[] math;
+
+    }
+
+}
+
