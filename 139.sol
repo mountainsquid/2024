830a155d9308dd6d784363f24f423190f0cc262f
Coding Sushi <123581788+mountainsquid@users.noreply.github.com>
2024-03-31 10:54:00 +0700

    Create 139.sol

---
+++ 139.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+// date: MARCH 31 2024
+
+pragma solidity ^0.8.25;
+
+contract HelloWorld {
+   
+   // in solidity you can have a contract deploying another contract 
+   // in solidity it's all about contracts interacting with other contracts like little bots 
+   // if it's EVM like avalanche, binance smart chain, Polygon then you can write soildity code for it
+   // import keyword, what does that do in solidity? 
+   // ABI = application binary interface, but what is it? 
+   // Opcodes are low-level machine instructions
+
+
+}
